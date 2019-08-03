e8930e1282e10a25720aa23f471081e8a87c5d34
Karthiek Naagaraj <knaaga90@gmail.com>
2019-08-03 19:08:12 -0400

    Date features updated
    Days are extracted from both formats now
    Removed unnecessary libraries
    Lead zeroes in dates (02,01,etc) removed

--- gmail.py
+++ gmail.py
@@ -1,11 +1,8 @@
-import smtplib
 import imapclient
 import pyzmail
-import pprint
-import bs4
-import urllib
 import time
 import datetime
+import calendar
 
 from openpyxl import Workbook
 from openpyxl import load_workbook
@@ -14,24 +11,13 @@ username = 'karthiek.umich@gmail.com'
 #password = input("enter password : ")
 password = 'amgmercedesM1596.2'
 
-# try:
-#     smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
-#     smtpobj.starttls()
-#     smtpobj.login(username,password)
-#     for i in range(0,10):
-#         smtpobj.sendmail(username,'knaaga90@gmail.com','Hello Buddy')
-#     smtpobj.quit()
-#
-# except:
-#     print("Something went wrong...")
-
 imapobj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
 imapobj.login(username, password)
 
 start_time = time.time()
 
 imapobj.select_folder('INBOX', readonly=True)
-UIDs = imapobj.search(['SINCE', '01-Jan-2019', 'BEFORE', '01-May-2019'])
+UIDs = imapobj.search(['SINCE', '01-Jul-2019', 'BEFORE', '30-Jul-2019'])
 print (str(len(UIDs)) + " emails recieved in this time frame")
 
 from_addresses = []
@@ -43,44 +29,43 @@ years = []
 times = []
 unsub_links = [None]*20000
 
-
-for i in range(100):
+for i in range(len(UIDs)):
     raw_message = imapobj.fetch(UIDs[i], ['BODY.PEEK[HEADER]'])
     message = pyzmail.PyzMessage.factory(raw_message[UIDs[i]][b'BODY[HEADER]'])
     from_addresses.append(message.get_address('from'))
     subjects.append(message.get_subject(''))
     full_date = message.get_decoded_header('date')
-    
 
     if (',' in full_date):
-        days.append(full_date.split(', ')[0])
-        dates.append((full_date.split(', ')[1].split()[0]))
-        months.append((full_date.split(', ')[1].split()[1]))
-        years.append((full_date.split(', ')[1].split()[2]))
-        times.append((full_date.split(', ')[1].split()[3]))
+        day = full_date.split(', ')[0]
+        date = full_date.split(', ')[1].split()[0]
+        if date[0] == '0':
+            date = date.split()[0][1:]
+        month = full_date.split(', ')[1].split()[1]
+        year = full_date.split(', ')[1].split()[2]
+        time2 = full_date.split(', ')[1].split()[3]
     else:
-        days.append('Unknown')
-        dates.append(full_date.split()[0])
-        months.append(full_date.split()[1])
-        years.append(full_date.split()[2])
-        times.append(full_date.split()[3])
+        date = full_date.split()[0]
+        if date[0] == '0':
+            date = date.split()[0][1:]
+        month = full_date.split()[1]
+        year = full_date.split()[2]
+        time2 = full_date.split()[3]
+        month_no = list(calendar.month_abbr).index(month)
+        day = (calendar.day_name[datetime.date(int(year), int(month_no), int(date)).weekday()]).split()[0][0:3]
 
-
-
+    days.append(day)
+    dates.append(date)
+    months.append(month)
+    years.append(year)
+    times.append(time2)
 
     unsub_links[i] = message.get_decoded_header('List-Unsubscribe')
-
-
-
-
-
 
 end_time = time.time()
 print ("execution time : " + str((end_time-start_time)))
 
-
 imapobj.logout()
-
 
 wb = Workbook()
 ws = wb.active
@@ -96,7 +81,7 @@ ws.cell(1,8).value = "Subject"
 ws.cell(1,9).value = "Unsubscribe Link"
 
 print(len(unsub_links))
-for i in range(100):
+for i in range(len(UIDs)):
 
     ws.cell(row=i+2, column=1).value = dates[i]
     ws.cell(row=i+2, column=2).value = months[i]
@@ -108,4 +93,3 @@ for i in range(100):
     ws.cell(row = i+2, column = 8).value = str(subjects[i])
     ws.cell(row=i+2, column=9).value = unsub_links[i]
 wb.save('Email_Analytics.xlsx')
-
