4430490b26ae7aa773f5ebcf00157f97dd0e2e04
Karthiek Naagaraj <knaaga90@gmail.com>
2019-08-04 19:13:17 -0400

    Multiple folder search and labels
    1. Searching multiple folders
    2. Checks for email label (string detection is very crude now. Needs to be done with regex)
    3. Checks whether email is a sent message or recieved message
    4. Progress bar added
    5. Code cleanup, comments included

--- gmail.py
+++ gmail.py
@@ -3,23 +3,25 @@ import pyzmail
 import time
 import datetime
 import calendar
+import re
+import pyprind
 
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
+# starting execution time counter
+start_time = time.time()
+
+# login credentials
 username = 'karthiek.umich@gmail.com'
 #password = input("enter password : ")
 password = 'amgmercedesM1596.2'
 
+# specify client and login
 imapobj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
 imapobj.login(username, password)
 
-start_time = time.time()
-
-imapobj.select_folder('INBOX', readonly=True)
-UIDs = imapobj.search(['SINCE', '01-Jul-2019', 'BEFORE', '30-Jul-2019'])
-print (str(len(UIDs)) + " emails recieved in this time frame")
-
+# initializing lists to hold various types of information
 from_addresses = []
 subjects = []
 dates = []
@@ -27,46 +29,108 @@ days = []
 months = []
 years = []
 times = []
-unsub_links = [None]*20000
+category = []
+sent_received = []
+unsub_links = []
+total_UIDs = 0
 
-for i in range(len(UIDs)):
-    raw_message = imapobj.fetch(UIDs[i], ['BODY.PEEK[HEADER]'])
-    message = pyzmail.PyzMessage.factory(raw_message[UIDs[i]][b'BODY[HEADER]'])
-    from_addresses.append(message.get_address('from'))
-    subjects.append(message.get_subject(''))
-    full_date = message.get_decoded_header('date')
+# specify search folders
+folder_list = ['[Gmail]/All Mail', '[Gmail]/Spam', '[Gmail]/Trash']
 
-    if (',' in full_date):
-        day = full_date.split(', ')[0]
-        date = full_date.split(', ')[1].split()[0]
-        if date[0] == '0':
-            date = date.split()[0][1:]
-        month = full_date.split(', ')[1].split()[1]
-        year = full_date.split(', ')[1].split()[2]
-        time2 = full_date.split(', ')[1].split()[3]
-    else:
-        date = full_date.split()[0]
-        if date[0] == '0':
-            date = date.split()[0][1:]
-        month = full_date.split()[1]
-        year = full_date.split()[2]
-        time2 = full_date.split()[3]
-        month_no = list(calendar.month_abbr).index(month)
-        day = (calendar.day_name[datetime.date(int(year), int(month_no), int(date)).weekday()]).split()[0][0:3]
+for folder in folder_list:
+    imapobj.select_folder(folder, readonly=True)
+    UIDs = imapobj.search(['SINCE', '02-Aug-2019', 'BEFORE', '09-Aug-2019'])
 
-    days.append(day)
-    dates.append(date)
-    months.append(month)
-    years.append(year)
-    times.append(time2)
+    current_UIDs = len(UIDs)
+    total_UIDs += len(UIDs)
 
-    unsub_links[i] = message.get_decoded_header('List-Unsubscribe')
+    # progress bar set by number of emails in current folder
+    n = current_UIDs
+    bar = pyprind.ProgBar(n)
 
-end_time = time.time()
-print ("execution time : " + str((end_time-start_time)))
+    if folder == '[Gmail]/All Mail':
+        print ("Analyzing all folders...")
+    elif folder == '[Gmail]/Spam':
+        print("Analyzing spam...")
+    elif folder == '[Gmail]/Trash':
+        print("Analyzing trash...")
 
+    for i in range(len(UIDs)):
+
+        # get email label
+        label_dict = imapobj.get_gmail_labels(UIDs[i])
+
+        # need to clean up using regex
+        label = str(label_dict.get(UIDs[i])).strip('[').strip(']').replace('\'','').replace('\\','')
+
+        if folder == '[Gmail]/Spam':
+            category.append('Spam')
+        elif folder == '[Gmail]/Trash':
+            category.append('Trash')
+        else:
+            if label == 'Inbox':
+                category.append('Inbox')
+            elif label == 'Sent':
+                category.append('Sent Mail')
+            elif 'Starred' in label:
+                category.append('Starred')
+            elif 'Important' in label:
+                category.append('Important')
+            else:
+                category.append('Custom Label')
+
+        raw_message = imapobj.fetch(UIDs[i], ['BODY.PEEK[HEADER]'])
+        message = pyzmail.PyzMessage.factory(raw_message[UIDs[i]][b'BODY[HEADER]'])
+
+        # check whether email is a sent message or a received message
+        if message.get_address('from')[1] == username:
+            sent_received.append('Sent')
+        else:
+            sent_received.append('Received')
+
+        from_addresses.append(message.get_address('from'))
+        subjects.append(message.get_subject(''))
+        full_date = message.get_decoded_header('date')
+
+        # two types of date formats are possible
+        if (',' in full_date):
+            day = full_date.split(', ')[0]
+            date = full_date.split(', ')[1].split()[0]
+            if date[0] == '0':
+                date = date.split()[0][1:]
+            month = full_date.split(', ')[1].split()[1]
+            year = full_date.split(', ')[1].split()[2]
+            time2 = full_date.split(', ')[1].split()[3]
+        else:
+            date = full_date.split()[0]
+            if date[0] == '0':
+                date = date.split()[0][1:]
+            month = full_date.split()[1]
+            year = full_date.split()[2]
+            time2 = full_date.split()[3]
+            month_no = list(calendar.month_abbr).index(month)
+            day = (calendar.day_name[datetime.date(int(year), int(month_no), int(date)).weekday()]).split()[0][0:3]
+
+        days.append(day)
+        dates.append(date)
+        months.append(month)
+        years.append(year)
+        times.append(time2)
+
+        unsub_link = message.get_decoded_header('List-Unsubscribe')
+        if len(str(unsub_link)) > 0:
+            unsub_links.append(unsub_link)
+        else:
+            unsub_links.append('No unsubscribe link found')
+
+        # update progress bar after a small delay
+        time.sleep(0.01)
+        bar.update()
+
+# logout of client
 imapobj.logout()
 
+# write extracted information to an excel sheet
 wb = Workbook()
 ws = wb.active
 ws.title = "Data"
@@ -78,11 +142,11 @@ ws.cell(1,5).value = "Time"
 ws.cell(1,6).value = "From (Sender)"
 ws.cell(1,7).value = "From (Email ID)"
 ws.cell(1,8).value = "Subject"
-ws.cell(1,9).value = "Unsubscribe Link"
+ws.cell(1,9).value = "Sent/Received"
+ws.cell(1,10).value = "Category"
+ws.cell(1,11).value = "Unsubscribe Link"
 
-print(len(unsub_links))
-for i in range(len(UIDs)):
-
+for i in range(total_UIDs):
     ws.cell(row=i+2, column=1).value = dates[i]
     ws.cell(row=i+2, column=2).value = months[i]
     ws.cell(row=i+2, column=3).value = years[i]
@@ -91,5 +155,15 @@ for i in range(len(UIDs)):
     ws.cell(row = i+2, column = 6).value = from_addresses[i][0]
     ws.cell(row = i+2, column = 7).value = from_addresses[i][1]
     ws.cell(row = i+2, column = 8).value = str(subjects[i])
-    ws.cell(row=i+2, column=9).value = unsub_links[i]
+    ws.cell(row=i + 2, column=9).value = sent_received[i]
+    ws.cell(row=i+2, column=10).value = category[i]
+    ws.cell(row=i + 2, column=11).value = unsub_links[i]
+
 wb.save('Email_Analytics.xlsx')
+
+# ending execution time counter
+end_time = time.time()
+print ("\n")
+print (str(total_UIDs) + " emails analyzed")
+print ("execution time : " + str((end_time-start_time)))
+
